96a624cf1a07c9b91b8f959fb3db167978454167
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-05 20:29:03 +0100

    a

--- booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -11,6 +11,12 @@ $(".fas.fa-bars").click(function() {
   $(".hamburger-menu").show();
   });
 
+$(".fas.fa-times").click(function() {
+  $(".hamburger-menu").hide();
+  });
+
+
+
 // $(".hamburger-menu").show();
 
 
