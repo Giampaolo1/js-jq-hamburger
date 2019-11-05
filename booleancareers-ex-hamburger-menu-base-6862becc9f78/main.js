88e516f220f128381d170c33da74ffe6b56d0008
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-05 19:16:53 +0100

    a

---
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,18 @@
+// DESCRIZIONE:
+//  Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript.
+// https://bitbucket.org/booleancareers/ex-hamburger-menu-base
+// La soluzione Base, concerne il fatto di non toccare html e css del repo di partenza
+// ma fare quello che dobbiamo seguendo quindi la logica implementata da chi ha fatto quell’intefaccia.
+
+$("div").header-right()
+
+
+// ● $(‘selector’).addClass(‘nomeclasse’): aggiunge la classe “nomeclasse” all’elemento selezionato
+// ● $(‘selector’).hide() : nasconde un elemento
+// ● $(‘selector’).click() : associa ad un click una funzione
+
+
+// $(‘p’).action() = tutti i <p>
+// $(‘p.intro’).action() = Seleziona tutti i <p> con classe intro
+// $(‘p:first).action() = Seleziona il primo elemento <p>
+// $(‘[href]’).action() = Seleziona tutti gli elementi con un attributo href
